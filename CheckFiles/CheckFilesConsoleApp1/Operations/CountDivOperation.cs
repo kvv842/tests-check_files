40feb6b9f04650e6376c07013be556afa5285915
Кириленко Виктор <kirilenkovv@it2g.ru>
2021-01-13 13:23:09 +0300

    Fix

--- CheckFiles/CheckFilesConsoleApp1/Operations/CountDivOperation.cs
+++ CheckFiles/CheckFilesConsoleApp1/Operations/CountDivOperation.cs
@@ -4,7 +4,7 @@ namespace CheckFilesConsoleApp1.Operations
 {
     public class CountDivOperation : IOperation
     {
-        private const string PATTERN = "<([div]+)(?:[^>=]|='[^']*'|=\"[^\"]*\" |=[^'\"\\s]*)*\\s?\\/?>";
+        private const string PATTERN = "<(div+)(?:[^>=]|='[^']*'|=\"[^\"]*\" |=[^'\"\\s]*)*\\s?\\/?>";
 
         private Regex _regex = new Regex(PATTERN);
 
